b91dda4a073e3cc6b8c12096d51e3683690ead70
LeKo <leopardorocha@gmail.com>
2024-05-23 16:13:06 -0300

    ADD

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\EmpresasController;
 use App\Http\Controllers\EspecialidadeController;
 use App\Http\Controllers\PacientesController;
 use App\Http\Controllers\PermisoesController;
@@ -45,7 +46,7 @@ Route::get('/especialidades/{id}', [EspecialidadeController::class, 'show'])->na
 #Usuarios
 
 Route::get('/usuarios', [UserController::class, 'index'])->name('usuario.index');
-Route::get('/lista', [UserController::class, 'index1'])->name('usuario.index1');
+Route::get('/listausuarios', [UserController::class, 'index1'])->name('usuario.index1');
 Route::post('/usuarios', [UserController::class, 'store'])->name('usuario.store');
 Route::put('/usuarios/{id}', [UserController::class, 'update'])->name('usuario.update');
 Route::delete('/usuarios/{id}', [UserController::class, 'destroy'])->name('usuario.destroy');
@@ -54,8 +55,17 @@ Route::get('/usuarios/{id}', [UserController::class, 'show'])->name('usuario.sho
 #Pacientes
 
 Route::get('/pacientes', [PacientesController::class, 'index'])->name('paciente.index');
-Route::get('/lista', [PacientesController::class, 'index1'])->name('paciente.index1');
+Route::get('/listapacientes', [PacientesController::class, 'index1'])->name('paciente.index1');
 Route::post('/pacientes', [PacientesController::class, 'store'])->name('paciente.store');
 Route::put('/pacientes/{id}', [PacientesController::class, 'update'])->name('paciente.update');
 Route::delete('/pacientes/{id}', [PacientesController::class, 'destroy'])->name('paciente.destroy');
 Route::get('/pacientes/{id}', [PacientesController::class, 'show'])->name('paciente.show');
+
+#Empresas
+
+Route::get('/empresas', [EmpresasController::class, 'index'])->name('empresa.index');
+// Route::get('/listaempresas', [EmpresasController::class, 'index1'])->name('empresa.index1');
+Route::post('/empresas', [EmpresasController::class, 'store'])->name('empresa.store');
+Route::put('/empresas/{id}', [EmpresasController::class, 'update'])->name('empresa.update');
+Route::delete('/empresas/{id}', [EmpresasController::class, 'destroy'])->name('empresa.destroy');
+Route::get('/empresas/{id}', [EmpresasController::class, 'show'])->name('empresa.show');
