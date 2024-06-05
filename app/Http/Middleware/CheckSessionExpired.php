f02f00fa85f5f9319b27ed7b398d0067573df335
LeKo <leopardorocha@gmail.com>
2024-06-05 16:04:04 -0300

    ADD

--- app/Http/Middleware/CheckSessionExpired.php
+++ app/Http/Middleware/CheckSessionExpired.php
@@ -10,10 +10,13 @@ class CheckSessionExpired
     public function handle($request, Closure $next)
     {
         if (Auth::check()) {
-            if (time() - $request->session()->get('last_activity', time()) > config('session.lifetime') * 1200) {
+            $lastActivity = $request->session()->get('last_activity', time());
+
+            if (time() - $lastActivity > config('session.lifetime') * 60) {
                 Auth::logout();
                 return redirect()->route('error.419');
             }
+
             $request->session()->put('last_activity', time());
         }
 
