31d9b9d075e0394e017e2c40a1e7eacd898da41c
gabryel329 <gabryel329@gmail.com>
2024-08-13 14:09:49 -0300

    ADD

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -15,7 +15,7 @@
             <ul class="dropdown-menu settings-menu dropdown-menu-right">
                 {{-- <li><a class="dropdown-item" href="page-user.html"><i class="bi bi-gear me-2 fs-5"></i> Settings</a></li> --}}
                 {{-- <li><a class="dropdown-item" href="page-user.html"><i class="bi bi-person me-2 fs-5"></i> Perfil</a></li> --}}
-                <li><a class="dropdown-item" href="#"><i class="bi bi-headset"></i> Suporte</a></li>
+                <li><a class="dropdown-item" href="http://esuporte.com.br:8090"><i class="bi bi-headset"></i> Suporte</a></li>
                 @if(Auth::user()->permissoes->contains('id', 3))
                     <li><a class="dropdown-item" href="{{ route('painelConsultorio.index') }}" target="_blank"><i class="bi bi-person-vcard"></i> Tela de Chamado</a></li>
                 @endif
